a17d54dd38cf97a575123c3d4dd4cddf2de66e84
wootaepark <pakdal99@gmail.com>
2024-11-14 14:30:58 +0900

    Lv1-5 commit

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -1,14 +1,20 @@
 package org.example.expert.domain.todo.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +22,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TodoController {
 
-    private final TodoService todoService;
+	private final TodoService todoService;
 
-    @PostMapping("/todos")
-    public ResponseEntity<TodoSaveResponse> saveTodo(
-            @Auth AuthUser authUser,
-            @Valid @RequestBody TodoSaveRequest todoSaveRequest
-    ) {
-        return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
-    }
+	@PostMapping("/todos")
+	public ResponseEntity<TodoSaveResponse> saveTodo(
+		@Auth AuthUser authUser,
+		@Valid @RequestBody TodoSaveRequest todoSaveRequest
+	) {
+		return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
+	}
 
-    @GetMapping("/todos")
-    public ResponseEntity<Page<TodoResponse>> getTodos(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
-    }
+	@GetMapping("/todos")
+	public ResponseEntity<Page<TodoResponse>> getTodos(
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(required = false) String weather,
+		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyyMMddHHmmss") LocalDateTime start,
+		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyyMMddHHmmss")LocalDateTime end
+	) {
 
-    @GetMapping("/todos/{todoId}")
-    public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
-        return ResponseEntity.ok(todoService.getTodo(todoId));
-    }
+		return ResponseEntity.ok(todoService.getTodos(page, size, weather, start, end));
+	}
+
+	@GetMapping("/todos/{todoId}")
+	public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
+		return ResponseEntity.ok(todoService.getTodo(todoId));
+	}
 }
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,5 +1,10 @@
 package org.example.expert.domain.todo.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -12,6 +17,7 @@ import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,6 +28,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class TodoService {
 
+    private final EntityManager em;
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
@@ -47,12 +54,35 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime startTime, LocalDateTime endTime) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        String jpql = "select t from Todo t where 1=1";
+        if(weather != null){
+            jpql += " and t.weather = :weather";
+        }
+        if(startTime != null){
+            jpql += " and t.modifiedAt >= :startTime";
+        }
+        if(endTime != null){
+            jpql += " and t.modifiedAt <= :endTime";
+        }
 
-        return todos.map(todo -> new TodoResponse(
+        TypedQuery<Todo> query = em.createQuery(jpql, Todo.class);
+        if (weather != null) query.setParameter("weather", weather);
+        if (startTime != null) query.setParameter("startTime", startTime);
+        if (endTime != null) query.setParameter("endTime", endTime);
+
+        query.setFirstResult((int)pageable.getOffset());
+        query.setMaxResults(pageable.getPageSize());
+
+        List<Todo> todos = query.getResultList();
+
+
+
+
+        List<TodoResponse> todoResponses = todos.stream().map(
+            todo -> new TodoResponse(
                 todo.getId(),
                 todo.getTitle(),
                 todo.getContents(),
@@ -60,7 +90,9 @@ public class TodoService {
                 new UserResponse(todo.getUser().getId(), todo.getUser().getNickname(), todo.getUser().getEmail()),
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
-        ));
+            )).toList();
+
+        return new PageImpl<>(todoResponses, pageable, todoResponses.size());
     }
 
     public TodoResponse getTodo(long todoId) {
